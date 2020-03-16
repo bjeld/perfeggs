fcc968fe975986436f9859d66380c308b52555c0
bjeld <martinbjeld@gmail.com>
2020-03-16 21:24:40 +0100

    Initial Commit

---
+++ perfeggs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  perfeggs
+//
+//  Created by Martin Bjeld on 16/03/2020.
+//  Copyright © 2020 Martin Bjeld. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
